9abd90edd0f5401bd3a55c9e283b9b14da18fdcf
giang <conmeobeo96@gmail.com>
2017-02-17 21:54:59 +0200

    Feat: added UI features, finished calculating arclength algoritm UNDONE: working on get path from a time interval

--- app/src/main/java/metro/ourthingsee/activities/LocationActivity.java
+++ app/src/main/java/metro/ourthingsee/activities/LocationActivity.java
@@ -1,9 +1,14 @@
 package metro.ourthingsee.activities;
 
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,17 +32,38 @@ import retrofit2.Response;
 
 public class LocationActivity extends AppCompatActivity {
     GoogleMap mGoogleMap;
+    ProgressDialog progressDialog;
+    View query_view;
+    FloatingActionButton fab_show_path, fab_current_location;
+    TextView tv_startDate, tv_startTime, tv_endDate, tv_endTime;
+    Button btn_showPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        Log.e("Giang",""+calculateArcLengthBaseOnLatLng(new LatLng(39.9,116.4), new LatLng(-31.95,115.867)));
         addControls();
     }
 
     private void addControls() {
         MapFragment mapFragment = (MapFragment)
                 getFragmentManager().findFragmentById(R.id.fragment_map);
+        progressDialog = new ProgressDialog(LocationActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Getting current location...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCanceledOnTouchOutside(false);
+        query_view = findViewById(R.id.query_view);
+        query_view.setVisibility(View.INVISIBLE);
+        fab_show_path = (FloatingActionButton) findViewById(R.id.fab_show_path);
+        fab_current_location = (FloatingActionButton) findViewById(R.id.fab_current_location);
+        fab_current_location.hide();
+        tv_startDate = (TextView) findViewById(R.id.tv_startDate);
+        tv_startTime = (TextView) findViewById(R.id.tv_startTime);
+        tv_endDate = (TextView) findViewById(R.id.tv_endDate);
+        tv_endTime = (TextView) findViewById(R.id.tv_endTime);
+        btn_showPath = (Button) findViewById(R.id.btn_showPath);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -48,8 +74,29 @@ public class LocationActivity extends AppCompatActivity {
                         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                         mGoogleMap.getUiSettings().setZoomGesturesEnabled(true);
                         getDeviceCurrentLocation(null);
+                        addEvents();
                     }
                 });
+            }
+        });
+    }
+
+    private void addEvents() {
+        fab_show_path.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                query_view.setVisibility(View.VISIBLE);
+                fab_show_path.hide();
+                fab_current_location.show();
+            }
+        });
+        fab_current_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                query_view.setVisibility(View.INVISIBLE);
+                fab_current_location.hide();
+                fab_show_path.show();
+                getDeviceCurrentLocation(null);
             }
         });
     }
@@ -60,6 +107,7 @@ public class LocationActivity extends AppCompatActivity {
     private void getDeviceCurrentLocation(Long endTimestamp) {
         SharedPreferences sharedPreferences = getSharedPreferences(OurContract.SHARED_PREF, MODE_PRIVATE);
         APIService apiService = AppUtils.getAPIService();
+        progressDialog.show();
         apiService.getUserEvents("Bearer "+sharedPreferences.getString(OurContract.PREF_USER_AUTH_TOKEN_NAME, "")
                 , sharedPreferences.getString(OurContract.PREF_DEVICE_AUTH_ID_NAME, "")
                 , "sense", "0x00010100,0x00010200", 1, null, endTimestamp)
@@ -85,6 +133,7 @@ public class LocationActivity extends AppCompatActivity {
                                 LatLng latLng = new LatLng(lat, lng);
                                 mGoogleMap.addMarker(new MarkerOptions().position(latLng));
                                 mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                                progressDialog.dismiss();
                             } else {
                                 //if sIds doesn't contain both desired values, call a recursion
                                 getDeviceCurrentLocation(response.body().getEvents().get(0).getTimestamp() - 1);
@@ -92,6 +141,7 @@ public class LocationActivity extends AppCompatActivity {
                         }else if (response.code() == 200 && response.body().getEvents().size() == 0){
                             //If there is no event contains the location, show a toast
                             Toast.makeText(LocationActivity.this, R.string.no_location, Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
                         }
                     }
 
@@ -101,7 +151,14 @@ public class LocationActivity extends AppCompatActivity {
                         Toast.makeText(LocationActivity.this,
                                 getString(R.string.login_toast_login_failed_nointernet),
                                 Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
                     }
                 });
+    }
+    private double calculateArcLengthBaseOnLatLng(LatLng start, LatLng end){
+        double distance = 2*6371*Math.asin(Math.sqrt((1-Math.sin(Math.toRadians(start.latitude))*Math.sin(Math.toRadians(end.latitude))
+                -Math.cos(Math.toRadians(start.latitude))*Math.cos(Math.toRadians(end.latitude))
+                *Math.cos(Math.toRadians(start.longitude-end.longitude)))/2));
+        return distance;
     }
 }
